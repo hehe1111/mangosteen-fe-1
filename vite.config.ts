33fbd626d7b00d998655acdd8e527a6e63064736
hehe1111 <2908749709@qq.com>
2023-08-09 13:37:35 +0800

    style: 删除无用引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import styleImport, { VantResolve } from 'vite-plugin-style-import';
